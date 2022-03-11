13090bb1aaf7c11f00855af734375f7db5c0b02c
Suraj <suraj569@gmail.com>
2022-03-11 16:07:08 +0530

    fourth commit

--- src/main/java/cicdtest/practice/App.java
+++ src/main/java/cicdtest/practice/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("TRigger build 1");
+        System.out.println("TRigger build 3");
     }
 }
