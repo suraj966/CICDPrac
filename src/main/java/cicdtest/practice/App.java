8926ef11599bae3ee1f4b67ada5cefbc35515883
Suraj <suraj569@gmail.com>
2022-03-11 12:47:24 +0530

    first commit

---
+++ src/main/java/cicdtest/practice/App.java
@@ -0,0 +1,13 @@
+package cicdtest.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
