5336a4594ef6f2a6caae6fcdcb18d10d5fb0517f
Suraj <suraj569@gmail.com>
2022-03-11 16:22:43 +0530

    sixth commit

--- src/main/java/cicdtest/practice/App.java
+++ src/main/java/cicdtest/practice/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("TRigger build 4");
+        System.out.println("TRigger build 5");
     }
 }
