08c14b38454242d765f01817a46cc4bd73c33976
Suraj <suraj569@gmail.com>
2022-03-11 17:51:52 +0530

    enive commit

--- src/main/java/cicdtest/practice/App.java
+++ src/main/java/cicdtest/practice/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("TRigger build 8");
+        System.out.println("TRigger build 9");
     }
 }
